3c5ee4f3987334aa02f0faac5fe7f94bb111ac24
unknown <Adnan>
2024-03-05 22:37:22 +0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+function addItem(e){
+    e.preventDefault();
+
+    const newItem = itemInput.value;
+    // Validate Input
+
+    if (newItem === ""){
+        alert ('Please add an item');
+        return;
+    }
+
+    /// Create List item
+
+   const li = document.createElement('li');
+   li.appendChild(document.createTextNode(newItem))
+
+   const button = createButton('remove-item btn-link text-red');
+
+   li.append(button)
+   itemList.append(li);
+   itemInput.value = '';
+}
+
+function createButton(classes){
+    const button = document.createElement('button')
+    button.className = classes;
+    const icon = createIcon('fa-solid fa-xmark')
+    button.append(icon)
+    return button;
+}
+function createIcon(classes){
+    const icon = document.createElement('i')
+    icon.className = classes;
+    return icon;
+}
+
+itemForm.addEventListener('submit', addItem)
+// console.log("ksmak");
